eb99428ea42df565bf617b1de3f7bebd614b2415
Andrew Suh <andrewsuh98@gmail.com>
2022-10-17 16:23:01 -0400

    set lspconfig settings file

--- .config/nvim/plugin/lspconfig.lua
+++ .config/nvim/plugin/lspconfig.lua
@@ -3,6 +3,7 @@ if (not status) then return end
 
 local protocol = require('vim.lsp.protocol')
 
+-- autocompletion per buffer
 local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
 local enable_format_on_save = function(_, bufnr)
 	vim.api.nvim_clear_autocmds({ group = augroup_format, buffer = bufnr })
@@ -15,57 +16,72 @@ local enable_format_on_save = function(_, bufnr)
 	})
 end
 
+-- global mappings
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+--vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-	local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-
-	--Enable completion triggered by <c-x><c-o>
-	--local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-	--buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
 	-- Mappings.
-	local opts = { noremap = true, silent = true }
-
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-	--buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-	buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-	--buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set('n', '<space>wl', function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, bufopts)
+	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+	vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+	local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
 end
 
-protocol.CompletionItemKind = {
-	'', -- Text
-	'', -- Method
-	'', -- Function
-	'', -- Constructor
-	'', -- Field
-	'', -- Variable
-	'', -- Class
-	'ﰮ', -- Interface
-	'', -- Module
-	'', -- Property
-	'', -- Unit
-	'', -- Value
-	'', -- Enum
-	'', -- Keyword
-	'﬌', -- Snippet
-	'', -- Color
-	'', -- File
-	'', -- Reference
-	'', -- Folder
-	'', -- EnumMember
-	'', -- Constant
-	'', -- Struct
-	'', -- Event
-	'ﬦ', -- Operator
-	'', -- TypeParameter
-}
+--protocol.CompletionItemKind = {
+--'', -- Text
+--'', -- Method
+--'', -- Function
+--'', -- Constructor
+--'', -- Field
+--'', -- Variable
+--'', -- Class
+--'ﰮ', -- Interface
+--'', -- Module
+--'', -- Property
+--'', -- Unit
+--'', -- Value
+--'', -- Enum
+--'', -- Keyword
+--'﬌', -- Snippet
+--'', -- Color
+--'', -- File
+--'', -- Reference
+--'', -- Folder
+--'', -- EnumMember
+--'', -- Constant
+--'', -- Struct
+--'', -- Event
+--'ﬦ', -- Operator
+--'', -- TypeParameter
+--}
 
 -- Set up completion using nvim_cmp with LSP source
-local capabilities = require('cmp_nvim_lsp').update_capabilities(
-	vim.lsp.protocol.make_client_capabilities()
-)
+--local capabilities = require('cmp_nvim_lsp').update_capabilities(
+--vim.lsp.protocol.make_client_capabilities()
+--)
 
 -- python
 nvim_lsp.pyright.setup {
@@ -75,7 +91,10 @@ nvim_lsp.pyright.setup {
 
 -- ocaml
 nvim_lsp.ocamllsp.setup {
-	on_attach = on_attach,
+	on_attach = function(client, bufnr)
+		on_attach(client, bufnr)
+		enable_format_on_save(client, bufnr)
+	end,
 	capabilities = capabilities
 }
 
@@ -102,28 +121,23 @@ nvim_lsp.sumneko_lua.setup {
 	},
 }
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-	vim.lsp.diagnostic.on_publish_diagnostics, {
+-- diagnostics config
+vim.diagnostic.config({
 	underline = true,
-	update_in_insert = false,
-	virtual_text = { spacing = 4, prefix = "●" },
-	severity_sort = true,
-}
-)
+	update_in_insert = true,
+	virtual_text = {
+		spacing = 4,
+		prefix = '●'
+	},
+	float = {
+		source = "always", -- Or "if_many"
+	},
+	severity_sort = true
+})
 
--- Diagnostic symbols in the sign column (gutter)
+---- Diagnostic symbols in the sign column (gutter)
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
 	local hl = "DiagnosticSign" .. type
 	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
-
-vim.diagnostic.config({
-	virtual_text = {
-		prefix = '●'
-	},
-	update_in_insert = true,
-	float = {
-		source = "always", -- Or "if_many"
-	},
-})
