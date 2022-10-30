f795b8c0830fb7ac00bf5e3bf3d97edb41ded23f
Andrew Suh <andrewsuh98@gmail.com>
2022-10-30 13:18:10 -0400

    modify installed plugs

--- .config/nvim/lua/andrewsuh/plugins.lua
+++ .config/nvim/lua/andrewsuh/plugins.lua
@@ -17,6 +17,7 @@ vim.cmd([[
 
 		" cmp plugins
 		Plug 'hrsh7th/nvim-cmp' " code completion
+		Plug 'hrsh7th/cmp-nvim-lsp' " nvim-cmp source for neovim's built-in LSP
 		Plug 'hrsh7th/cmp-buffer' " nvim-cmp source for buffer completion
 		" Plug 'hrsh7th/cmp-path' " nvim-cmp source for path completion
 		" Plug 'hrsh7th/cmp-cmdline' " nvim-cmp source for cmdline completion
@@ -26,8 +27,9 @@ vim.cmd([[
 		Plug 'L3MON4D3/LuaSnip' " snippet plugin
 
 		" LSP plugins
-		" Plug 'neovim/nvim-lspconfig' " lsp server configs
-		" Plug 'hrsh7th/cmp-nvim-lsp' " nvim-cmp source for neovim's built-in LSP
+		" Plug 'williamboman/mason.nvim' " LSP package manager
+		" Plug 'williamboman/mason-lspconfig.nvim' " hook for mason and neovim's LSP
+		Plug 'neovim/nvim-lspconfig' " lsp server configs
 
 	call plug#end()
 ]])
