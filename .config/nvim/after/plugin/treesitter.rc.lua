37bd9eb78ad0474842efb85a5259b161bae06342
Andrew Suh <andrewsuh98@gmail.com>
2022-11-02 14:53:35 -0400

    fix treesitter folding

--- .config/nvim/after/plugin/treesitter.rc.lua
+++ .config/nvim/after/plugin/treesitter.rc.lua
@@ -19,6 +19,11 @@ ts.setup {
 }
 
 -- enable folding with treesitter
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldenable = false
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufAdd', 'BufNew', 'BufNewFile', 'BufWinEnter' }, {
+	group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
+	callback = function()
+		vim.opt.foldmethod = 'expr'
+		vim.opt.foldexpr   = 'nvim_treesitter#foldexpr()'
+		vim.opt.foldenable = false
+	end
+})
