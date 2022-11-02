abaf03e11a5cf9477ddf53a8db5037885834523a
Andrew Suh <andrewsuh98@gmail.com>
2022-11-02 15:22:38 -0400

    add workaround for cmp need check nil

--- .config/nvim/after/plugin/cmp.rc.lua
+++ .config/nvim/after/plugin/cmp.rc.lua
@@ -1,5 +1,6 @@
 local cmp_status, cmp = pcall(require, "cmp")
 if (not cmp_status) then return end
+---@cast cmp -?
 
 local luasnip_status, luasnip = pcall(require, "luasnip")
 if (not luasnip_status) then return end
