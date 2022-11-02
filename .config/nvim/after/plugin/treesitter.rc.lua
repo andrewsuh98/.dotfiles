37686be0cbe48df67c74a1bc90e7093fb76ff9c7
Andrew Suh <andrewsuh98@gmail.com>
2022-11-02 14:44:04 -0400

    refactor

--- .config/nvim/after/plugin/treesitter.rc.lua
+++ .config/nvim/after/plugin/treesitter.rc.lua
@@ -2,7 +2,9 @@ local status, ts = pcall(require, "nvim-treesitter.configs")
 if (not status) then return end
 
 ts.setup {
-	ensure_installed = {'c', 'c_sharp', 'cmake', 'comment', 'cpp', 'css', 'dockerfile', 'gitignore', 'html', 'http','java', 'javascript', 'jsdoc', 'json', 'json5', 'latex', 'lua', 'make', 'markdown', 'markdown_inline', 'ocaml', 'ocaml_interface', 'python', 'regex', 'rust', 'solidity', 'sql', 'typescript', 'vim'},
+	ensure_installed = { 'c', 'c_sharp', 'cmake', 'comment', 'cpp', 'css', 'dockerfile', 'gitignore', 'html', 'http', 'java',
+		'javascript', 'jsdoc', 'json', 'json5', 'latex', 'lua', 'make', 'markdown', 'markdown_inline', 'ocaml',
+		'ocaml_interface', 'python', 'regex', 'rust', 'solidity', 'sql', 'typescript', 'vim' },
 	highlight = {
 		enable = true,
 		disable = {},
@@ -19,7 +21,4 @@ ts.setup {
 -- enable folding with treesitter
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
--- default to all expanded
-vim.cmd [[
-	autocmd BufReadPost,FileReadPost * normal zR
-]]
+vim.opt.foldenable = false
