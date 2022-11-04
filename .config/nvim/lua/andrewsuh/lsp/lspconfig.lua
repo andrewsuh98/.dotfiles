0f56e0d03cbc423831a5f6f7e7bdde86460c3484
Andrew Suh <andrewsuh98@gmail.com>
2022-11-03 21:04:17 -0400

    update lspconfig file

--- .config/nvim/lua/andrewsuh/lsp/lspconfig.lua
+++ .config/nvim/lua/andrewsuh/lsp/lspconfig.lua
@@ -14,30 +14,55 @@ local enable_format_on_save = function(_, bufnr)
 	})
 end
 
--- global keymaps
+local function lsp_highlight_document(client, bufnr)
+	if client.server_capabilities.documentHighlightProvider then
+		vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
+		vim.api.nvim_clear_autocmds { buffer = bufnr, group = "lsp_document_highlight" }
+		vim.api.nvim_create_autocmd("CursorHold", {
+			callback = vim.lsp.buf.document_highlight,
+			buffer = bufnr,
+			group = "lsp_document_highlight",
+			desc = "Document Highlight",
+		})
+		vim.api.nvim_create_autocmd("CursorMoved", {
+			callback = vim.lsp.buf.clear_references,
+			buffer = bufnr,
+			group = "lsp_document_highlight",
+			desc = "Clear All the References",
+		})
+	end
+end
+
+-- Diagnostics keymaps
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
---vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-	local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-
-	-- Mappings.
-	local bufopts = { noremap = true, silent = true }
-
+	-- LSP mappings
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', bufopts)
-	buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', bufopts)
-	buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', bufopts)
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set('n', '<space>k', vim.lsp.buf.signature_help, bufopts)
+	-- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+	-- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+	-- vim.keymap.set('n', '<space>wl', function()
+	-- 	print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	-- end, bufopts)
+	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 
-	-- formatting
-	vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
-
-	buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', bufopts)
+	lsp_highlight_document(client, bufnr)
 end
 
 -- Set up completion using nvim_cmp with LSP source
@@ -55,22 +80,15 @@ nvim_lsp.pyright.setup {
 
 -- ocaml
 nvim_lsp.ocamllsp.setup {
-	on_attach = function(client, bufnr)
-		on_attach(client, bufnr)
-		-- enable_format_on_save(client, bufnr)
-	end,
+	on_attach = on_attach,
 	capabilities = capabilities
 }
 
 -- c# with omnisharp
 nvim_lsp.omnisharp.setup {
 	-- path to omnisharp executable
-	cmd = { "dotnet", "/usr/local/bin/omnisharp-roslyn/OmniSharp.dll" },
-
-	on_attach = function(client, bufnr)
-		on_attach(client, bufnr)
-		-- enable_format_on_save(client, bufnr)
-	end,
+	cmd = { "dotnet", "/Users/andrewsuh/.local/share/nvim/mason/packages/omnisharp/OmniSharp.dll" },
+	on_attach = on_attach,
 	capabilities = capabilities
 }
 
@@ -97,28 +115,27 @@ nvim_lsp.sumneko_lua.setup {
 	},
 }
 
--- when publishing diagnostics
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-	vim.lsp.diagnostic.on_publish_diagnostics, {
-	underline = true,
-	update_in_insert = false,
-	virtual_text = { spacing = 4, prefix = "●" },
-	severity_sort = true,
-}
-)
-
 -- Diagnostics config
 vim.diagnostic.config({
 	underline = true,
-	update_in_insert = true,
+	update_in_insert = false,
 	virtual_text = {
 		spacing = 4,
 		prefix = '●'
 	},
 	float = {
+		border = "rounded",
 		source = "always", -- Or "if_many"
 	},
 	severity_sort = true
+})
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+	border = "rounded",
+})
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+	border = "rounded",
 })
 
 ---- Diagnostic symbols in the sign column (gutter)
