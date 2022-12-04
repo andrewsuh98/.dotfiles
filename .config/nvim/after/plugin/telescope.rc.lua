61f0e8ed8b99dc65d50ed2e7866935f844b66971
Andrew Suh <andrewsuh98@gmail.com>
2022-12-04 17:42:35 -0500

    add live grep hotkey to telescope

--- .config/nvim/after/plugin/telescope.rc.lua
+++ .config/nvim/after/plugin/telescope.rc.lua
@@ -5,6 +5,7 @@ if (not status) then return end
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<Leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<Leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<Leader>fG', builtin.grep_string, {})
 vim.keymap.set('n', '<Leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<Leader>fh', builtin.help_tags, {})
 
@@ -23,7 +24,7 @@ telescope.setup {
 				preview_width = 0.5,
 				preview_cutoff = 120
 			}
-		}
+		},
 	},
 	pickers = {
 		-- ...
